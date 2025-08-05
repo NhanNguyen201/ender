94953e5d7a1b2ab0680d1bcdfea38a76eb8b5cd2
nhannguyen201 <nhangc2000@gmail.com>
2025-08-06 02:21:35 +0700

    add impl take_damage to unit

--- src/unit.rs
+++ src/unit.rs
@@ -18,6 +18,12 @@ impl HealthExt for Health {
     fn get_current_hit_points(&self) -> f32 {
         self.current_hit_points
     }
+    fn take_damage(&mut self, damage: f32) {
+        self.current_hit_points -= damage;
+        if self.current_hit_points < 0.0 {
+            self.current_hit_points = 0.0;
+        }
+    }
 }
 
 #[derive(Component, Debug, Clone, Copy)]
@@ -34,6 +40,7 @@ impl AttackExt for Attack {
 #[derive(Component, Debug, Clone, Copy)]
 pub struct Mobility {
     pub speed: f32,
+    pub turn_speed: f32,
     pub direction: Vec3,
 }
 
@@ -41,18 +48,23 @@ impl Default for Mobility {
     fn default() -> Self {
         Mobility {
             speed: 0.0,
+            turn_speed: 0.0,
             direction: Vec3::ZERO,
         }
     }
 }
 
 impl MobilityExt for Mobility {
-    fn new(speed: f32, direction: Vec3) -> Self {
-        Mobility { speed, direction }
+    fn new(speed: f32, turn_speed: f32, direction: Vec3) -> Self {
+        Mobility { speed, turn_speed , direction }
     }
 
     fn get_speed(&self) -> f32 {
         self.speed
+    }
+
+    fn get_turn_speed(&self) -> f32 {
+        self.turn_speed
     }
 
     fn get_direction(&self) -> Vec3 {
