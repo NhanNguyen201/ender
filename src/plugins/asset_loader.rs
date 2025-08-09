c3c92db2a95eeaa54746bb8d003be99fef078cfb
nhannguyen201 <nhangc2000@gmail.com>
2025-08-09 15:43:33 +0700

    New asset koading plugin

--- src/plugins/asset_loader.rs
+++ src/plugins/asset_loader.rs
@@ -1,11 +1,14 @@
 use std::collections::HashMap;
 
-use bevy::{asset::LoadedFolder, prelude::*};
+use bevy::prelude::*;
+
+use crate::gameplay::GameState;
 
 
 #[derive(Resource, Default)]
 pub struct AssetPack {
     pub mesh_store: HashMap<String, Handle<Mesh>>,
+    pub loading_handles: Vec<UntypedHandle>,
     // pub scene_store: HashMap<String, Handle<Scene>>,
     // pub collision_shapes: HashMap<String, Handle<CollisionShape>>,
 }
@@ -16,7 +19,8 @@ impl Plugin for AssetLoaderPlugin {
     fn build(&self, app: &mut App) {
         app
             .init_resource::<AssetPack>()
-            .add_systems(PreStartup, load_asset);
+            .add_systems(OnEnter(GameState::LoadingAssets), load_asset)
+            .add_systems(Update, check_if_loaded.run_if(in_state(GameState::LoadingAssets)));
     }
 }
 fn insert_asset(
@@ -25,17 +29,30 @@ fn insert_asset(
     key: &str,
     mesh_path: &str,
 ) {
-    let mesh: Handle<Mesh> = asset_server.load(mesh_path);
+    let handle: Handle<Mesh> = asset_server.load(mesh_path);
     // let scene: Handle<Scene> = asset_server.load(scene_path);
-    asset_pack.mesh_store.insert(key.to_string(), mesh);
+    asset_pack.loading_handles.push(handle.clone().into());
+    asset_pack.mesh_store.insert(key.to_string(), handle);
     // asset_pack.scene_store.insert(key.to_string(), scene);
 }
-
+fn check_if_loaded (
+     asset_server: Res<AssetServer>,
+    asset_pack: Res<AssetPack>,
+    mut next_state: ResMut<NextState<GameState>>,
+){
+    if asset_pack
+            .loading_handles
+            .iter()
+            .all(|handle| asset_server.is_loaded(handle))
+        {
+            info!("✅ All assets loaded!");
+            next_state.set(GameState::StartupScreen);
+        }
+}
 fn load_asset(
     asset_server: Res<AssetServer>,
     mut asset_pack: ResMut<AssetPack>
 ) {
-    let _loaded_folder: Handle<LoadedFolder>= asset_server.load_folder("models");
 
      // Human ships
     insert_asset(&mut asset_pack, &asset_server, "human_mother_ship",
